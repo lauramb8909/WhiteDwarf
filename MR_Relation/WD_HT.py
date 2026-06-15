485590a9da6fc1a168cee5dbc3484c90c2486645
laura <laura.marcela.becerra@gmail.com>
2026-06-15 19:10:40 -0400

    fixing Hartle

--- MR_Relation/WD_HT.py
+++ MR_Relation/WD_HT.py
@@ -55,6 +55,8 @@ def TOV_HT(r,y, EOS, DEOS):
 
     rhons = EOS(pns)
     DrhodP = np.power(10.0, DEOS( np.log10(pns) ) )
+    rho2   = ( pns + rhons ) * y[6] * DrhodP
+    p2     = y[6] * ( pns + rhons )
 
     r2 = r*r
     r3 = r2*r 
@@ -66,23 +68,29 @@ def TOV_HT(r,y, EOS, DEOS):
     ec3 = -0.5 * ec2 * ( pns + rhons) #dpdr
     
     ec4 = y[3] #domegadr
-    ec5 = - 4.0 * y[3] / r + 4.0 * pii * r2 * ( pns + rhons ) * ( y[3] + 4.0 * y[4] / r ) / ( r - 2.0 * mns )
+    ec5 = - 4.0 * y[3] / r + 4.0 * pii * r * ( pns + rhons ) * ( y[3] * r + 4.0 * y[4] ) / ( r - 2.0 * mns )
 
     ### l=0 (second order in Omega)
-    ec6 = pi4 * r2 * ( pns + rhons ) * y[6] * DrhodP + ( r3 *nuns / 12.0 ) * y[3]**2.0 * ( r - 2.0*mns ) + (2.0*pi4 / 3.0) * y[4]**2.0  * ( pns + rhons ) *  r4* nuns #dm0dr
-    
-    ec7 = -y[5] * ( 1.0 + 2.0*pi4 *r2 * pns ) / ( r - 2.0*mns )**2.0 - y[6] *pi4 * r2 *( pns + rhons) / ( r - 2.0*mns) + ( nuns / 3.0) * ( y[4]**2*(2.0*r - r2*ec2) + 2.0*r2 * y[3] * y[4] + r3*y[3]**2 / 4.0 ) #dp0dr
+    ec6 = pi4 * r2 * rho2  + ( r3 *nuns / 12.0 ) * y[3]**2.0 * ( r - 2.0*mns ) + (2.0 * pi4 / 3.0) *r4 *nuns * ( pns + rhons ) * y[4]**2.0    #dm0dr
+
+    dh0 = y[5]*( 1.0 + 2.0 * pi4 * r2*pns ) / (r - 2.0 * mns)**2.0 + pi4 * r2 * p2 / ( r - 2.0*mns ) - r3*nuns * y[3]**2 / 12
+     
+    ec7 = -dh0 + ( nuns / 3.0) * ( y[4]**2 * ( 2.0*r - r2*ec2 ) + 2.0*r2 * y[3] * y[4] ) #dp0dr
 
     ####### l=2 (second order in Omega)
-    ec8 = -ec2 * y[8] +  ( 1.0 + 0.5* r * ec2 )*( (2.0*pi4/3.0) * r3 * y[4]**2.0 * ( pns + rhons ) 
-                                               + r2*( r - 2.0*mns ) * y[3]**2 / 6.0 ) * nuns # dvdr
+
+    m2  = -y[8] + (2.0*pi4*pii/3.0)* r4 * nuns * ( rhons + pns ) * y[4]**2.0  + (1.0/6.0) * r3 * nuns * ( r-2*mns) * y[3]**2.0
+
+    #ec8 = -ec2 * y[8] +  ( 1.0 + 0.5* r * ec2 )*( (2.0*pi4/3.0) * r3 * y[4]**2.0 * ( pns + rhons ) 
+    #                                           + r2*( r - 2.0*mns ) * y[3]**2 / 6.0 ) * nuns # dvdr
+
+    ec8 = y[8] * ( 1.0 / r - ec2 / 2.0 ) + m2 * ( 1.0 / r + ec2 / 2.0 ) 
     
     ec9 =  - y[8] * ec2 + 4.0 * y[8] * ( pi2 * ( pns + rhons ) * r3 - mns ) / ( ec2 * r2 * ( r - 2.0*mns ) ) - 2.0 * y[7] / ( mns + pi4 * r3 * pns ) + (1.0/6.0) * nuns  * y[3]**2.0 * ( 0.5 * r3 * ( r - 2.0*mns ) *ec2 - r2 / ec2 )+( pi4/3.0 )* r3 * y[4]**2.0 * ( pns + rhons ) * nuns * (  r * ec2 + 2.0 / ( (r - 2.0*mns ) * ec2)) #dh2dr
     
     ec10 = -y[10]*ec2 #dvdr (homogenea)
     
     ec11 = -y[10]* ec2 + 4.0 * y[10]*( pi2 * ( pns + rhons ) * r3 - mns ) / ( ec2 * r2 * ( r - 2.0*mns ) ) - 2.0 * y[9] / ( mns + pi4 * r3 * pns) #dh2dr (homogenea)
-    
     
     return np.array( [ ec1, ec3, ec2, ec5, ec4, ec6, ec7, ec8, ec9, ec10, ec11 ] )
 
@@ -114,39 +122,47 @@ def OmegaK( Mwd, Rwd, DM, jwd, qwd):
 
 #------ Functions to caculate the quadrupole
 def Q22(x):
-    x2 = x*x 
-    x3 = x2*x
-   
-    if x>1e3:    
-        x5  = x2*x3
-        x6  = x3*x3; x7= x6*x;  x9 = x6*x3
-        x11 = x9*x2; x12 = x6*x6
-        x13 = x12*x
-        x15 = x12*x3 
-        A   = 168.0 / ( 85.0 * x15 ) + 128.0 / ( 65.0 * x13 ) + 280.0 / ( 143.0 * x11 ) + 64.0/(33.0*x9)+ 40.0 / (21.0* x7 ) + 64.0 / ( 35.0 * x5 ) + 8.0 / (5.0*x3)
+    
+    if x>1e3: 
+        x1  = 1.0/x
+        x2  = x1 * x1
+        x3  = x1 * x2
+        x5  = x2 * x3
+        x6  = x3 * x3
+        x7  = x6 * x1 
+        x9  = x6 * x3
+        x11 = x9 * x2
+        x12 = x6 * x6
+        x13 = x12 * x1
+        x15 = x12 * x3 
+        A   = 168.0 * x15 / 85.0  + 128.0 * x13 / 65.0 + 280.0 * x11 / 143.0 + 64.0 * x9 / 33.0 + 40.0 * x7 / 21.0 + 64.0 * x5 / 35.0  + 8.0 * x3 / 5.0
     else:
+        x2 = x*x 
+        x3 = x2*x
         A   = (3.0/2.0)*( x2 - 1.0)*( np.log(x+1.0) - np.log(x-1.0) ) - ( 3.0*x3 - 5.0*x) /( x2 - 1.0 )
     return A
 
 
 def Q12(x):
-    x2 = x*x
-    x3 = x2*x
-
+    
     if x>1e3:
-        x5  = x2*x3
-        x6  = x3*x3 
-        x7  = x6*x
-        x9  = x6*x3
-        x11 = x9*x2
-        x12 = x6*x6
-        x13 = x12*x
-        x15 = x12*x3 
+        x1  = 1.0/x
+        x2  = x1 * x1
+        x3  = x1 * x2
+        x5  = x2 * x3
+        x6  = x3 * x3
+        x7  = x6 * x1 
+        x9  = x6 * x3
+        x11 = x9 * x2
+        x12 = x6 * x6
+        x13 = x12 * x1
+        x15 = x12 * x3 
 
-        A = 866483.0 / ( 3734016.0 * x15 ) + 95923.0 / ( 384384.0 * x13 ) + 52069.0 / ( 192192.0 * x11 ) + 2749.0 / ( 9240.0 * x9 ) + 139.0 / ( 420.0 * x7 ) + 13.0 / ( 35.0 * x5 ) + 2.0 /  ( 5.0*x3 )
+        A = 866483.0 * x15 / 3734016.0 + 95923.0 * x13 / 384384.0 + 52069.0 * x11 / 192192.0 + 2749.0 * x9 / 9240.0 + 139.0 * x7 / 420.0 + 13.0 * x5 / 35.0 + 2.0 *x3 / 5.0
     else:
+        x2 = x*x
+        x3 = x2*x
         A = ( 3.0 / 2.0 ) * x * np.sqrt( x2 - 1.0 ) * ( np.log( x - 1.0 ) - np.log( x + 1.0 ) ) + ( 3.0*x2 - 2.0 ) / np.sqrt( x2 - 1.0 )
-        #A = - (4.0-6.0*x2 + 3.0*x*(-1.0+x2)*np.log((1+x)/(1-x))/(2.0*np.sqrt(1.0-x2)))
     return A
 
 def Awd( jwd, Mwd, Rwd, h2h, h2p, v2h, v2p ):
@@ -262,7 +278,7 @@ def MassRadius( y0,drr,rff, eos, deos ):
     mstar     = test.y[0]
     rstar     = test.t
 
-    R2        = rstar*rstar
+    R2        = rstar * rstar
     R3        = R2 * rstar
     R4        = R2 * R2
     nuc       = test.y[2]
@@ -271,15 +287,17 @@ def MassRadius( y0,drr,rff, eos, deos ):
    
     J2        = Jstar*Jstar
     Mass      =  mstar + test.y[5] + J2 / R3 
-    epsilonz  = test.y[6] * rstar * ( rstar - 2.0 * mstar ) / ( mstar + 4.0*pii * R3 * test.y[1] )
 
+    dff       = TOV_HT(rstar,test.y, EoS_RFMT_02, DeDpF)
+    epsilonz  = test.y[6] * rstar * ( rhons + test.y[1]) / abs( dff[1] ) #* ( rstar - 2.0 * mstar ) / ( mstar + 4.0*pii * R3 * test.y[1] )
+    
     AAs       = Awd(Jstar,mstar,rstar,test.y[10],test.y[8],test.y[9],test.y[7])
     v2star    = test.y[7] + AAs[1]*test.y[9]
     h2star    = test.y[8] + AAs[1]*test.y[10]
     ppt       = -h2star - ( 1.0 / 3.0) * R2 * np.exp( -nuc ) * test.y[4]**2
-    epsilont  = ppt * rstar  * ( rstar - 2.0 * mstar ) / ( mstar + 4.0 * pii *R3 * test.y[1]  )
+    epsilont  = ppt * rstar * ( rhons + test.y[1]) / abs( dff[1] ) #* ( rstar - 2.0 * mstar ) / ( mstar + 4.0 * pii *R3 * test.y[1]  )
     #equatorial radius
-    RR        =  rstar + epsilonz - 0.5*epsilont
+    RR        =  rstar + epsilonz - 0.5*epsilont - 0.5 * rstar * ( h2star - v2star )
     
     Qstar     = J2 / mstar + (8.0/5.0) * mstar**3 * AAs[0]
 
